0ef8afae283377f60367686fba53784f605508fb
auggiemarignier <augustin.marignier@anu.edu.au>
2023-10-10 13:46:49 +1100

    put in both implementations

--- octo/basis.py
+++ octo/basis.py
@@ -22,21 +22,36 @@ class BaseBasis(metaclass=ABCMeta):
 
 
 class CosineBasis(BaseBasis):
-    def __init__(self, N: int, resolution: int = None) -> None:
+    def __init__(self, N: int, resolution: int = None, method="idct") -> None:
         super().__init__(N)
         self.resolution = (
             2 * N if resolution is None else resolution
         )  # number of points per cycle [0, 2pi)
+
+        if method not in ["idct", "cos"]:
+            raise ValueError("method must be one of 'idct', 'cos'")
+        self.method = (
+            self._basis_from_idct if method == "idct" else self._basis_from_cos
+        )
+
         self.create_basis()
 
     def create_basis(self) -> None:
         self.basis = np.zeros((self.N, self.resolution))
         for i in range(self.N):
-            self.basis[i, :] = idct(np.eye(self.resolution)[i, :], norm="ortho")
+            self.basis[i, :] = self.method(i)
 
     def compute_jacobian(self, forward) -> None:
         self.jacobian = np.array([[forward(0.0)], [forward(1.0)]])
         return self.jacobian
+
+    def _basis_from_idct(self, i) -> np.ndarray:
+        return idct(np.eye(self.resolution)[i, :], norm="ortho")
+
+    def _basis_from_cos(self, i) -> np.ndarray:
+        return np.cos(i * np.linspace(0, np.pi, self.resolution)) / np.sqrt(
+            self.resolution / 2
+        )
 
 
 class PixelBasis(BaseBasis):
