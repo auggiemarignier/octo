a3aac8d30bd647ac0f5784776294c8705b0afc53
auggiemarignier <augustin.marignier@anu.edu.au>
2023-10-10 16:27:00 +1100

    parameterise tests

--- tests/test_basis.py
+++ tests/test_basis.py
@@ -3,6 +3,21 @@ import pytest
 from octo.basis import CosineBasis, PixelBasis, CosineBasis2D, PixelBasis2D
 
 
+@pytest.fixture
+def N():
+    return 10
+
+
+@pytest.fixture
+def Nx():
+    return 10
+
+
+@pytest.fixture
+def Ny():
+    return 5
+
+
 def _is_othornomal_basis(basis_matrix):
     N = basis_matrix.shape[0]
     for i in range(N):
@@ -12,40 +27,20 @@ def _is_othornomal_basis(basis_matrix):
             assert np.isclose(dot_product, expected), f"{i,j}"
 
 
-def test_cosine_basis_implementations():
-    N = 20
+def test_cosine_basis_implementations(N):
     res = 10000
     b1 = CosineBasis(N, res, method="idct")
     b2 = CosineBasis(N, res, method="cos")
     assert np.allclose(b1.basis, b2.basis)
 
 
-@pytest.mark.parametrize("method", ["idct", "cos"])
-def test_cosine_basis_orthonormal(method):
-    N = 10
-    cosine_basis = CosineBasis(N, method=method)
-    basis_matrix = cosine_basis.basis
-    _is_othornomal_basis(basis_matrix)
+@pytest.mark.parametrize("basis", [PixelBasis, CosineBasis])
+def test_1D_basis_orthonormal(basis, N):
+    _basis = basis(N)
+    _is_othornomal_basis(_basis.basis)
 
 
-def test_pixel_basis_orthonormal():
-    N = 10
-    pixel_basis = PixelBasis(N)
-    basis_matrix = pixel_basis.basis
-    _is_othornomal_basis(basis_matrix)
-
-
-def test_cosine2d_basis_orthonormal():
-    Nx = 10
-    Ny = 10
-    cosine2d_basis = CosineBasis2D(Nx, Ny)
-    basis_matrix = cosine2d_basis.basis
-    _is_othornomal_basis(basis_matrix)
-
-
-def test_pixel2d_basis_orthonormal():
-    Nx = 10
-    Ny = 5
-    pixel2d_basis = PixelBasis2D(Nx, Ny)
-    basis_matrix = pixel2d_basis.basis
-    _is_othornomal_basis(basis_matrix)
+@pytest.mark.parametrize("basis", [PixelBasis2D, CosineBasis2D])
+def test_2d_basis_orthonormal(basis, Nx, Ny):
+    _basis = basis(Nx, Ny)
+    _is_othornomal_basis(_basis.basis)
