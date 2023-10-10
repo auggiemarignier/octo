86c5b6c4e88c22aaac50351751d2b7f23a21dc6f
auggiemarignier <augustin.marignier@anu.edu.au>
2023-10-10 13:59:49 +1100

    refactor orthonormal test

--- tests/test_basis.py
+++ tests/test_basis.py
@@ -3,6 +3,17 @@ import pytest
 from octo.basis import CosineBasis, PixelBasis
 
 
+def _is_othornomal_basis(basis_matrix):
+    N = basis_matrix.shape[0]
+    for i in range(N):
+        for j in range(N):
+            dot_product = np.dot(basis_matrix[i, :], basis_matrix[j, :])
+            if i == j:
+                assert np.isclose(dot_product, 1.0), f"{i,j}"
+            else:
+                assert np.isclose(dot_product, 0.0), f"{i,j}"
+
+
 def test_cosine_basis_implementations():
     N = 20
     res = 10000
@@ -16,23 +27,11 @@ def test_cosine_basis_orthonormal(method):
     N = 10
     cosine_basis = CosineBasis(N, method=method)
     basis_matrix = cosine_basis.basis
-    for i in range(N):
-        for j in range(N):
-            dot_product = np.dot(basis_matrix[i, :], basis_matrix[j, :])
-            if i == j:
-                assert np.isclose(dot_product, 1.0), f"{i,j}"
-            else:
-                assert np.isclose(dot_product, 0.0), f"{i,j}"
+    _is_othornomal_basis(basis_matrix)
 
 
 def test_pixel_basis_orthonormal():
     N = 10
     pixel_basis = PixelBasis(N)
     basis_matrix = pixel_basis.basis
-    for i in range(N):
-        for j in range(N):
-            dot_product = np.dot(basis_matrix[i, :], basis_matrix[j, :])
-            if i == j:
-                assert np.isclose(dot_product, 1.0), f"{i,j}"
-            else:
-                assert np.isclose(dot_product, 0.0), f"{i,j}"
+    _is_othornomal_basis(basis_matrix)
