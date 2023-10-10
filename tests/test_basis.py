47170ddcfb0a016596851cb5b19a3e6bca7a31c1
auggiemarignier <augustin.marignier@anu.edu.au>
2023-10-10 13:54:20 +1100

    test the two implementations

--- tests/test_basis.py
+++ tests/test_basis.py
@@ -3,46 +3,18 @@ import pytest
 from octo.basis import CosineBasis, PixelBasis
 
 
-@pytest.mark.parametrize(
-    "basis_class, N, M, expected_basis",
-    [
-        (CosineBasis, 3, 2, np.array([[1.0, 0.5, 0.0], [1.0, 0.0, -1.0]])),
-        (
-            PixelBasis,
-            4,
-            3,
-            np.array(
-                [[1.0, 0.0, 0.0, 0.0], [0.0, 1.0, 0.0, 0.0], [0.0, 0.0, 1.0, 0.0]]
-            ),
-        ),
-    ],
-)
-def test_basis_attribute(basis_class, N, M, expected_basis):
-    basis = basis_class(N, M)
-    assert np.allclose(basis.basis, expected_basis)
-
-
-def test_plot_cosine_basis_functions():
-    import matplotlib.pyplot as plt
-
+def test_cosine_basis_implementations():
     N = 20
     res = 10000
-    cosine_basis = CosineBasis(N, res)
-    basis_matrix = cosine_basis.basis
-    b2 = np.zeros_like(basis_matrix)
-    for i in range(N):
-        b2[i, :] = np.cos(i * np.linspace(0, np.pi, res)) / np.sqrt(res / 2)
-        if i == 0:
-            b2[i, :] /= np.sqrt(2)
-        plt.plot(i + basis_matrix[i, :])
-        plt.plot(i + b2[i, :], ls="--")
-    plt.show()
-    assert np.allclose(basis_matrix, b2, rtol=1e-3, atol=1e-8)
+    b1 = CosineBasis(N, res, method="idct")
+    b2 = CosineBasis(N, res, method="cos")
+    assert np.allclose(b1.basis, b2.basis)
 
 
-def test_cosine_basis_orthonormal():
+@pytest.mark.parametrize("method", ["idct", "cos"])
+def test_cosine_basis_orthonormal(method):
     N = 10
-    cosine_basis = CosineBasis(N)
+    cosine_basis = CosineBasis(N, method=method)
     basis_matrix = cosine_basis.basis
     for i in range(N):
         for j in range(N):
