f8770fa9409d41c72b22bcc64e6e2915cb37486c
auggiemarignier <augustin.marignier@anu.edu.au>
2023-10-10 11:29:44 +1100

    test function to quickly plot the basis functions

--- tests/test_basis.py
+++ tests/test_basis.py
@@ -20,3 +20,15 @@ from octo.basis import CosineBasis, PixelBasis
 def test_basis_attribute(basis_class, N, M, expected_basis):
     basis = basis_class(N, M)
     assert np.allclose(basis.basis, expected_basis)
+
+
+def test_plot_cosine_basis_functions():
+    import matplotlib.pyplot as plt
+
+    N = 10
+    res = 100
+    cosine_basis = CosineBasis(N, res)
+    basis_matrix = cosine_basis.basis
+    for i in range(N):
+        plt.plot(i + basis_matrix[i, :])
+    plt.show()
