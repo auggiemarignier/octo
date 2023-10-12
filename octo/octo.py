1dfa5206fa561741053933089dde94272a509d98
auggiemarignier <augustin.marignier@anu.edu.au>
2023-10-12 16:34:22 +1100

    add weights and jaobian checking functions

--- octo/octo.py
+++ octo/octo.py
@@ -1,17 +1,39 @@
 from octo.basis import BaseBasis, CosineBasis, PixelBasis
 import numpy as np
 from scipy.optimize import minimize
-from typing import List
+from typing import List, Callable
 
 
 class OvercompleteBasis:
-    def __init__(self, bases: List[BaseBasis]) -> None:
+    def __init__(
+        self,
+        bases: List[BaseBasis],
+        bweights: List[float] = None,
+        rweight: float = None,
+    ) -> None:
+        """
+        bases: list of basis objects
+        bweights: list of weights for each basis.  Default is 1.0 for each basis.
+        rweight: Regularisation weight. Default is 1.0.
+        """
         self.bases = bases
+        self.bweights = bweights if bweights is not None else [1.0 for _ in bases]
+        self.rweight = rweight if rweight is not None else 1.0
+
+        self._check_jacobians()
 
     def cost(self, x: float, y: float) -> float:
         basis_val = self.basis.create_basis()
         jacobian = self.basis.compute_jacobian(forward=lambda x: x**2)
         return np.sum((y - basis_val) ** 2) + np.sum(jacobian)
+
+    def update_jacobians(self, forward: Callable):
+        for basis in self.bases:
+            basis.compute_jacobian(forward)
+
+    def _check_jacobians(self):
+        for basis in self.bases:
+            assert basis.jacobian is not None, "Jacobian not computed for basis"
 
 
 def minimize_cost(overcomplete_basis: OvercompleteBasis, x0: float, y: float) -> float:
