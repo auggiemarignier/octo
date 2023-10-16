28d78c963dcd4e3229d0c8ef15c122a18f8bc921
auggiemarignier <augustin.marignier@anu.edu.au>
2023-10-17 10:26:27 +1100

    sphinx quickstart

---
+++ docs/source/conf.py
@@ -0,0 +1,29 @@
+from octo import __version__
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "octo"
+copyright = "2023, Auggie Marignier"
+author = "Auggie Marignier"
+version = __version__
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["sphinx.ext.autodoc"]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
