a630189e347a9c247985f9919744bd1ce7e17184
auggiemarignier <augustin.marignier@anu.edu.au>
2023-10-17 16:07:43 +1100

    config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,3 +31,8 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 html_theme = "sphinx_book_theme"
 html_static_path = ["_static"]
+
+html_theme_options = {
+    "repository_url": "https://github.com/auggiemarignier/octo",
+    "use_repository_button": True,
+}
